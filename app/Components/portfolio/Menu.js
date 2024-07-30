8efe356866e6a16592bcab2aa84a0be771d599d0
Helios <130355604+HydRen-fr@users.noreply.github.com>
2024-07-30 19:51:44 +0200

    Update Menu.js

--- app/Components/portfolio/Menu.js
+++ app/Components/portfolio/Menu.js
@@ -2,45 +2,21 @@
 const Menu =[
   {
     id:1,
-    Image: "https://i.ibb.co/LxcHc59/NIKE-LANDING-PAGE.png",
-    title:"Nike Landing Page",
-    category: "Design"
+    Image: "/public/undistract.png",
+    title:"Undistract add-on",
+    category: "Firefox add-on"
   },
   {
     id:2,
-    Image: "https://i.ibb.co/2tWsbWh/Hero-Section.jpg",
-    title:"Online Course selling Website Design",
-    category: "Design"
+    Image: "/public/epydemie.png",
+    title:"Epydemie",
+    category: "Simulator"
   },
   {
     id:3,
-    Image: "https://i.ibb.co/Wk2KTqs/RickShaw.jpg",
-    title:"RickShaw APP Design",
-    category: "Design"
-  },
-  {
-    id:4,
-    Image: "https://i.ibb.co/zP8Srzm/Screenshot-2023-09-24-182118.png",
-    title:"Modern Chair Showcase APP",
-    category: "Applications"
-  },
-  {
-    id:5,
-    Image: "https://i.ibb.co/Yfys33S/weather-app.png",
-    title:"Weather APP",
-    category: "Applications"
-  },
-  {
-    id:6,
-    Image: "https://i.ibb.co/0GtVbHq/dev-detetc.png",
-    title:"Dev Detection",
-    category: "Applications"
-  },
-  {
-    id:7,
-    Image: "https://i.ibb.co/zspXP6d/flank.png",
-    title:"FLANK a gaming app ",
-    category: "Case Study"
+    Image: "/public/flora.png",
+    title:"Flora",
+    category: "React website"
   },
 ]
 export default Menu
