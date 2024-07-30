0187d1f6b23fdeaa4b62011949fc2a0ac328984c
Helios <130355604+HydRen-fr@users.noreply.github.com>
2024-07-30 19:12:53 +0200

    Update Contact.js

--- app/Components/contact/Contact.js
+++ app/Components/contact/Contact.js
@@ -8,7 +8,6 @@ const Contact = () => {
       <div className='contact__container grid'>
         <div className='contact__info'>
           <h3 className='contact__title'> Let's talk!</h3>
-          <p className='contact__details'>Don't like forms? Send me an email.</p>
         </div>
 
         <form action='' className='contact__form'>
