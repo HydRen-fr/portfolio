bb1019b3aa739620c6c3e995fa0b8919dbd598f1
Helios <130355604+HydRen-fr@users.noreply.github.com>
2024-07-30 21:20:00 +0200

    Update Contact.js

--- app/Components/contact/Contact.js
+++ app/Components/contact/Contact.js
@@ -5,32 +5,9 @@ const Contact = () => {
   return (
     <section className='contact container section' id='contact'>
       <h2 className='section__title'>Get in touch</h2>
-      <div className='contact__container grid'>
-        <div className='contact__info'>
-          <h3 className='contact__title'> Let's talk!</h3>
-        </div>
-
-        <form action="mailto:hbringuet@numericable.fr" method="post" enctype="text/plain" className='contact__form'>
-          <div className='contact__form-group'>
-            <div className='contact__form-div'>
-              <input type='text' className='contact__form-input' placeholder='Your name'/>
-            </div>
-            
-            <div className='contact__form-div'>
-              <input type='email' className='contact__form-input' placeholder='Insert your email'/>
-            </div>
-          </div>
-
-          <div className='contact__form-div'>
-              <input type='text' className='contact__form-input' placeholder=''/>
-            </div>
-
-            <div className='contact__form-div contact__form-area'>
-              <textarea name='' id='' cols={30} rows={10} className='contact__form-input' placeholder=''></textarea>
-            </div>
-             <button className='btn'>Send message</button>
-        </form>
-      </div>
+      &nbsp;
+      <h3 className='section__title'>hbringuet@numericable.fr</h3>
+      <h3 className='section__title'>Follow me on instagram or linkedin</h3>
     </section>
   )
 }
