33a7dbe38ec6ed26fc79de8e152f1bbf413eb64f
Helios <130355604+HydRen-fr@users.noreply.github.com>
2024-07-30 21:31:24 +0200

    Update Contact.js

--- app/Components/contact/Contact.js
+++ app/Components/contact/Contact.js
@@ -6,8 +6,8 @@ const Contact = () => {
     <section className='contact container section' id='contact'>
       <h2 className='section__title'>Get in touch</h2>
       &nbsp;
-      <h3 className='section__title'>hbringuet@numericable.fr</h3>
-      <h3 className='section__title'>Follow me on instagram or linkedin</h3>
+      <h4>hbringuet@numericable.fr</h4>
+      <h4>Follow me on instagram or linkedin</h4>
     </section>
   )
 }
